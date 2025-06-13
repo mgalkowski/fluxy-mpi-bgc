81297c6b1cb2d23740320f648b1ac33ae7a5178b
Leania98 <herrfurth.leah@gmail.com>
2025-06-13 11:19:33 +0200

    added more variables to combine and rename

--- mpi-bgc-intern/utils/csr_preprocessor.py
+++ mpi-bgc-intern/utils/csr_preprocessor.py
@@ -1,5 +1,6 @@
 import xarray as xr
 
+
 rename_candidates = {
     'longitude': ['lon'],
     'latitude' : ['lat'],
@@ -8,12 +9,27 @@ rename_candidates = {
     'area' : ['cell_area']
 }
 
-def preprocess(prior_path, posterior_path, output_path):
+def preprocess(path_to_prior, path_to_posterior, path_to_output):
 
-    ds = xr.open_dataset(prior_path)
-    ds = _rename(ds)
-    ds = _combine_flux_total(ds, xr.open_dataset(posterior_path))
-    ds.to_netcdf(output_path)
+    ds_prior = xr.open_dataset(path_to_prior)
+    ds_posterior = xr.open_dataset(path_to_posterior)
+    ds_prior = _rename(ds_prior)
+    ds_posterior = _rename(ds_posterior)
+    ds = _combine_variable(ds_prior, ds_posterior,
+                       varnames=["co2flux_land", "co2flux_ocean"],
+                       new_prior_name="flux_total_prior",
+                       new_post_name="flux_total_posterior",
+                       sum_vars=True,
+                       drop_originals=True)
+    ds = _combine_variable(ds, ds_posterior,
+                       varnames=["co2flux", "ch4flux"],
+                       new_prior_name="flux_total_prior_country",
+                       new_post_name="flux_total_posterior_country")
+    ds = _combine_variable(ds, ds_posterior,
+                       varnames=["co2flux_unc"],
+                       new_prior_name="stdevflux_total_prior_country",
+                       new_post_name="stdev_flux_total_posterior_country")
+    ds.to_netcdf(path_to_output)
 
 
 
@@ -24,7 +40,7 @@ def _rename(ds):
 
     for target_name, possible_names in rename_candidates.items():
         for name in possible_names:
-            if name in ds.dims:
+            if name in ds.dims or name in ds.coords or name in ds.data_vars:
                 rename_dict[name] = target_name
                 break  # use the first match only
     
@@ -39,10 +55,90 @@ def _combine_flux_total(ds_prior, ds_posterior):
     
     ds = ds_prior
     flux_to_sum = ['co2flux_land', 'co2flux_ocean']
-    flux_total_prior = sum(ds[flux] for flux in flux_to_sum)
-    ds['flux_total_prior'] = flux_total_prior
-    ds = ds.drop_vars(flux_to_sum)
-    ds = ds.drop_vars('co2flux_subt')
-    flux_total_posterior = sum(ds_posterior[flux] for flux in flux_to_sum)
-    ds['flux_total_posterior'] = flux_total_posterior
+
+     # Only sum existing variables
+    prior_fluxes = [ds[flux] for flux in flux_to_sum if flux in ds]
+    posterior_fluxes = [ds_posterior[flux] for flux in flux_to_sum if flux in ds_posterior]
+
+    # Create new variables only if the sum is not empty
+    if prior_fluxes:
+        ds['flux_total_prior'] = sum(prior_fluxes)
+
+    if posterior_fluxes:
+        ds['flux_total_posterior'] = sum(posterior_fluxes)
+
+    # Drop only variables that exist
+    vars_to_drop = [v for v in flux_to_sum if v in ds]
+    if 'co2flux_subt' in ds:
+        vars_to_drop.append('co2flux_subt')
+
+    if vars_to_drop:
+        ds = ds.drop_vars(vars_to_drop)
+
     return ds
+
+def _combine_flux_total_country(ds_prior, ds_posterior):
+    possible_names = {"co2flux", "ch4flux"}
+    ds = ds_prior
+    for name in possible_names:
+        if name in ds.data_vars:
+            ds = ds.rename({name: "flux_total_prior_country"})
+            break  # use the first match only
+    
+    for name in possible_names:
+        if name in ds_posterior.data_vars:
+            ds["flux_total_posterior_country"] = ds_posterior[name]
+            break
+    return ds
+
+
+def _combine_stdev_flux_total_country(ds_prior, ds_posterior):
+    possible_names = {"co2flux_unc"}
+    ds = ds_prior
+    for name in possible_names:
+        if name in ds.data_vars:
+            ds = ds.rename({name: "stdev_flux_total_prior_country"})
+            break  # use the first match only
+    
+    for name in possible_names:
+        if name in ds_posterior.data_vars:
+            ds["stdev_flux_total_posterior_country"] = ds_posterior[name]
+            break
+    return ds
+
+
+
+def _combine_variable(ds_prior, ds_posterior, varnames, new_prior_name, new_post_name, sum_vars=False, drop_originals=False):
+    ds = ds_prior.copy()
+
+    if sum_vars:
+        prior_data = [ds[v] for v in varnames if v in ds]
+        post_data  = [ds_posterior[v] for v in varnames if v in ds_posterior]
+
+        if prior_data:
+            ds[new_prior_name] = sum(prior_data)
+        if post_data:
+            ds[new_post_name] = sum(post_data)
+    else:
+        for name in varnames:
+            if name in ds.data_vars:
+                ds = ds.rename({name: new_prior_name})
+                break
+        for name in varnames:
+            if name in ds_posterior.data_vars:
+                ds[new_post_name] = ds_posterior[name]
+                break
+
+    if drop_originals:
+        varnames.append("co2flux_subt")
+        vars_to_drop = [v for v in varnames if v in ds]
+        ds = ds.drop_vars(vars_to_drop)
+      
+
+    return ds
+
+
+
+
+
+    
