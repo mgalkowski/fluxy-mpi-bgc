d9f464deef4f77757e6921543bfe09251b777b79
Joe Pitt <zh21490@bristol.ac.uk>
2025-07-02 10:50:17 +0000

    remove unneccessary combine_map_dataset hack

--- fluxy/operators/flux_combine.py
+++ fluxy/operators/flux_combine.py
@@ -38,7 +38,6 @@ def combine_dataset(
 
 
 def combine_map_dataset(ds_all: dict[str, xr.Dataset]) -> dict[str, xr.Dataset]:
-
     """
     Combine multiple xarray datasets along the 'model' dimension and return the mean dataset.
 
@@ -48,10 +47,6 @@ def combine_map_dataset(ds_all: dict[str, xr.Dataset]) -> dict[str, xr.Dataset]:
     Returns:
         A dictionary with a single key 'combined', containing the mean of all datasets along the 'model' dimension.
     """
-    for d in list(ds_all.keys()):
-        for v in ds_all[d]:
-            if '_out' in v or 'percentile_flux_total_posterior_inversion_grid' in v:
-                ds_all[d] = ds_all[d].drop_vars(v,errors='ignore')
 
     models = list(ds_all.keys())
     ds_list = list(ds_all.values())
