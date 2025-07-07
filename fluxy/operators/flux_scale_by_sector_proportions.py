ada60efdad3b364026951c0ee16c1ea6b45a01ad
Alice Ramsden <alice.ramsden@metoffice.gov.uk>
2025-07-07 17:21:36 +0100

    added region sector flux calculation - needs to be sped up

--- fluxy/operators/flux_scale_by_sector_proportions.py
+++ fluxy/operators/flux_scale_by_sector_proportions.py
@@ -2,6 +2,8 @@ import logging
 import os
 import numpy as np
 import xarray as xr
+from pathlib import Path
+from fluxy.operators.convert import get_units_conversion_factor
 
 logger = logging.getLogger(__name__)
 
@@ -16,14 +18,79 @@ def sum_region_fluxes(ds,vars,regions):
     
     return ds
 
-def scale_by_sector_proportions(datadir,ds):
+def scale_by_sector_proportions(data_dir:str,
+                                ds: xr.Dataset,
+                                species: str,
+                                sectors: list[str],
+                                regions: list[str],
+                                country_flux_units_print: str,
+                                config_data = dict[str, dict],
+                                sector_proportions_file: str = 'EUROPE_EDGAR',
+                                create_region_sector_totals: bool = True
+) -> xr.Dataset:
     """
     Produces sector level fluxes by scaling prior and posterior fluxes by the 
     proportional contribution of each sector to the total flux in each grid cell
     (at transport model resolution). Fluxes are then summed over region/country areas
     to produce region/country sector totals.    
+    
+    Args:
+        data_dir (str):
+            Path to top data directory.
+        ds (xarray dataset):
+            xarray dataset with model data.
+        species (str):
+            Gas species, e.g. 'ch4'.
+        sectors (list of str):
+            Emissions sectors to include, options for 'agriculture', 'waste', 
+            'energy' and 'industry'.
+        regions (list of str):
+            Region names. Sector region/country totals are only calculated for
+            these areas.
+        country_flux_units_print (str):
+            Units for country flux, e.g. 'Gg yr-1'
+        config_data (dict):
+            config_data (dict of dict):
+            Dictionary with settings read from json file.
+            Use json filenames as keys.
+        sector_proportions_file (str):
+            Start of sector_proportions file name, e.g. 'EUROPE_EDGAR'
+        create_region_sector_totals (bool):
+            If True, sums spatial fluxes over country_fraction masks 
+            to create country/region totals.
+    
+    Returns:
+        ds (xarray dataset):
+            Input ds, with added flux_sector_prior and flux_sector_posterior variables.
+            If regions is not None, also contains region/country sector total variables.
     """
     
+    if create_region_sector_totals == True:
+        
+        if 'country_fraction' not in ds:
+            raise ValueError(
+                f"country_fraction variable not present in dataset for {ds['inversion_system']} "
+                "Cannot add sector country totals."
+                )
+            
+        if 'cell_area' in ds:
+            cell_area = ds['cell_area'].values
+        else:
+            parent_dir = Path(__file__).parent.parent.parent
+            configs_dir = parent_dir / "configs"
+            if 'domain' in ds.attrs:
+                domain = ds.attrs["domain"]
+            else:
+                domain = sector_proportions_file.split('_')[0]
+                logger.warning(f'No domain info in dataset attributes, so reading domain from sector_proportions_filename: {domain}')
+            with xr.open_dataset(os.path.join(configs_dir,f"{domain}_cell_area.nc")) as f:
+                cell_area = f['cell_area'].values
+                
+    #units for spatial flux to regional flux totals
+    molar_mass = config_data["species_info"][species]["molar_mass"]
+    s_in_year = 60*60*24*365
+    scaling_factor = get_units_conversion_factor('mol s-1', country_flux_units_print, molar_mass)
+     
     sector_prop_path = os.path.join(data_dir,'sector_proportions',
                                        f'{sector_proportions_file}_{species}_yearly_flux_sector_proportions.nc')
 
@@ -39,6 +106,9 @@ def scale_by_sector_proportions(datadir,ds):
         flux_sector_prior_out = np.zeros_like(ds['flux_total_prior'].values)
         flux_sector_post_out = np.zeros_like(ds['flux_total_posterior'].values)
         
+        region_sector_prior_out = np.zeros_like(ds['flux_total_prior_country'].values)
+        region_sector_post_out = np.zeros_like(ds['flux_total_posterior_country'].values)
+        
         for i,t in enumerate(ds.time.values):
             # matches sector proportions times to inversion times
             if t.astype('datetime64[Y]').astype('datetime64[ns]') in sector_time: 
@@ -53,20 +123,38 @@ def scale_by_sector_proportions(datadir,ds):
 
             flux_sector_prior_out[i,:,:] = ds[f'flux_total_prior'].values[i,:,:]*scaling_factor
             flux_sector_post_out[i,:,:] = ds[f'flux_total_posterior'].values[i,:,:]*scaling_factor
+
+            if create_region_sector_totals == True:
+                #TODO: add in region calc only for listed countries, to speed up this function
+                for r,region_search in enumerate(ds['country'].values):
+                    r_fraction = ds['country_fraction'].values[r,:,:]
+                    region_sector_prior_out[i,r] = np.nansum(flux_sector_prior_out[i,:,:]*r_fraction
+                                                            *cell_area*scaling_factor)
+                    region_sector_post_out[i,r] = np.nansum(flux_sector_prior_out[i,:,:]*r_fraction
+                                                            *cell_area*scaling_factor)
             
-        ds[f'flux_{s}_prior'] = (['time','lat','lon'],sector_flux_out)
+            
+        ds[f'flux_{s}_prior'] = (['time','lat','lon'],flux_sector_prior_out)
         ds[f'flux_{s}_prior'].attrs = {'unit':'mol m-2 s-1',
                                             '_FillValue':np.nan,
                                             'long_name':f'prior {s} {species} flux, created by scaling total flux by sector proportions'}
         
-        ds[f'flux_{s}_posterior'] = (['time','lat','lon'],sector_flux_out)
+        ds[f'flux_{s}_posterior'] = (['time','lat','lon'],flux_sector_post_out)
         ds[f'flux_{s}_posterior'].attrs = {'unit':'mol m-2 s-1',
                                             '_FillValue':np.nan,
                                             'long_name':f'posterior total {species} {s} flux, created by scaling total flux by sector proportions'}
         
-    #sum over sliced regions to create region/country totals for each sector - using country fraction in flux ds
-    # use function above
-    
-    
+        if create_region_sector_totals == True:
+        
+            ds[f'flux_{s}_prior_country'] = (['time','country'],region_sector_prior_out)
+            ds[f'flux_{s}_prior_country'].attrs = {'unit':'kg yr-1',
+                                                '_FillValue':np.nan,
+                                                'long_name':f'country {s} {species} prior flux, created by scaling total flux by sector proportions'}
+
+            ds[f'flux_{s}_posterior_country'] = (['time','country'],region_sector_post_out)
+            ds[f'flux_{s}_posterior_country'].attrs = {'unit':'kg yr-1',
+                                                '_FillValue':np.nan,
+                                                'long_name':f'country {s} {species} posterior flux, created by scaling total flux by sector proportions'}
+
     return ds
 
